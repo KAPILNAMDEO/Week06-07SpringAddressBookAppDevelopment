9cfdf2b0fb0514656836ea54d388f53fb9387a93
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-03-06 12:50:02 +0530

    [Kapil_Namdeo]Add. logger @Slf4j

--- src/main/java/com/bridgelabz/SpringAddressBook/SpringAddressBookApplication.java
+++ src/main/java/com/bridgelabz/SpringAddressBook/SpringAddressBookApplication.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.SpringAddressBook;
 
+
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringAddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAddressBookApplication.class, args);
-	}
+		log.info("Starting Address Book Application...");
 
+		SpringApplication.run(SpringAddressBookApplication.class, args);
+
+		log.info("Address Book Application started successfully.");
+	}
 }
